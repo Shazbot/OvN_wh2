3c98076f27795901f850ceea93ff9423cfb65044
Tibor Ilić <tilic5@gmail.com>
2021-01-29 20:38:16 +0100

    adjusted halfling agents UI script overwriting

--- ovn_main_scripts/script/campaign/mod/ovn_faction_halflings.lua
+++ ovn_main_scripts/script/campaign/mod/ovn_faction_halflings.lua
@@ -17,34 +17,55 @@ local function halflings_init()
 
     setup_diplo()
 
-    core:add_listener(
-				"pj_moot_rename_chorf_agents",
-				"PanelOpenedCampaign",
-				function(context)
-						return context.string == "character_panel"
-				end,
-				function()
-						if cm:get_local_faction_name(true) ~= "wh2_main_emp_the_moot" then return end
+		core:remove_listener("pj_moot_rename_hlf_agents")
+		core:add_listener(
+			"pj_moot_rename_hlf_agents",
+			"PanelOpenedCampaign",
+			function(context)
+					return context.string == "character_panel"
+			end,
+			function()
+					if cm:get_local_faction_name(true) ~= "wh2_main_emp_the_moot" then return end
 
-						local ui_root = core:get_ui_root()
-						local list_box = find_uicomponent(ui_root, "character_panel", "agent_parent", "list_clip", "holder", "list_box")
+					local ui_root = core:get_ui_root()
+					local list_box = find_uicomponent(ui_root, "character_panel", "agent_parent", "list_clip", "holder", "list_box")
 
-						if not list_box then return end
+					if not list_box then return end
 
-						local agent_label = find_uicomponent(list_box, "champion", "label")
-						if agent_label then
-								agent_label:SetStateText("Hero")
-						end
-						agent_label = find_uicomponent(list_box, "spy", "label")
-						if agent_label then
-								agent_label:SetStateText("Protector")
-						end
-						agent_label = find_uicomponent(list_box, "dignitary", "label")
-						if agent_label then
-								agent_label:SetStateText("Guardian")
-						end
-				end,
-				true
+					local champion = find_uicomponent(list_box, "champion")
+					local champion_label = champion and find_uicomponent(champion, "label")
+					if champion_label then
+							local agent_desc = effect.get_localised_string("agent_subtypes_description_text_override_ovn_hlf_sheriff")
+							local existing_tooltip_text = champion:GetTooltipText()
+
+							existing_tooltip_text = agent_desc.."\n\n"..existing_tooltip_text
+							champion:SetTooltipText(existing_tooltip_text, true)
+							champion_label:SetStateText(effect.get_localised_string("ovn_hlf_ui_champion_label"))
+					end
+
+					local spy = find_uicomponent(list_box, "spy")
+					local spy_label = spy and find_uicomponent(spy, "label")
+					if spy_label then
+							local agent_desc = effect.get_localised_string("agent_subtypes_description_text_override_ovn_hlf_fieldwarden")
+							local existing_tooltip_text = spy:GetTooltipText()
+
+							existing_tooltip_text = agent_desc.."\n\n"..existing_tooltip_text
+							spy:SetTooltipText(existing_tooltip_text, true)
+							spy_label:SetStateText(effect.get_localised_string("ovn_hlf_ui_spy_label"))
+					end
+
+					local dignitary = find_uicomponent(list_box, "dignitary")
+					local dignitary_label = dignitary and find_uicomponent(dignitary, "label")
+					if dignitary_label then
+							local agent_desc = effect.get_localised_string("agent_subtypes_description_text_override_ovn_hlf_master_chef")
+							local existing_tooltip_text = dignitary:GetTooltipText()
+
+							existing_tooltip_text = agent_desc.."\n\n"..existing_tooltip_text
+							dignitary:SetTooltipText(existing_tooltip_text, true)
+							dignitary_label:SetStateText(effect.get_localised_string("ovn_hlf_ui_dignitary_label"))
+					end
+			end,
+			true
 		)
 
     if faction_obj:is_human() then
