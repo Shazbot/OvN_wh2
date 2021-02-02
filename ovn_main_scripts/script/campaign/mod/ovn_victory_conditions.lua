4c3343d5809bd5bef5992a138d3ee25c1140e2dc
Tibor Ilić <tilic5@gmail.com>
2021-02-02 18:39:57 +0100

    changed victory condition to collect all ingredients

--- ovn_main_scripts/script/campaign/mod/ovn_victory_conditions.lua
+++ ovn_main_scripts/script/campaign/mod/ovn_victory_conditions.lua
@@ -293,50 +293,30 @@ function ovn_victory_conditions()
                    --wh2_main_emp_the_moot
         elseif cm:get_faction("wh2_main_emp_the_moot"):is_human() then
 
-            local mission = [[
-                     mission
-                    {
-                        victory_type ovn_victory_type_long;
-                        key wh_main_long_victory;
-                        issuer CLAN_ELDERS;
-                        primary_objectives_and_payload
-                        {
-                            objective
-                            {
-                            type CONTROL_N_REGIONS_FROM;
-                            total 17;
-                            region wh_main_couronne_et_languille_couronne;
-                            region wh_main_reikland_altdorf;
-                            region wh_main_eastern_sylvania_castle_drakenhof;
-                            region wh_main_yn_edri_eternos_the_oak_of_ages;
-                            region wh_main_tilea_miragliano;
-                            region wh2_main_skavenblight_skavenblight;
-                            region wh_main_southern_oblast_kislev;
-                            region wh2_main_hell_pit_hell_pit;
-                            region wh_main_the_silver_road_karaz_a_karak;
-                            region wh_main_eastern_badlands_karak_eight_peaks;
-                            region wh_main_death_pass_karak_drazh;
-                            region wh2_main_devils_backbone_lahmia;
-                            region wh2_main_land_of_the_dead_khemri;
-                            region wh2_main_southern_great_jungle_itza;
-                            region wh2_main_isthmus_of_lustria_hexoatl;
-                            region wh2_main_iron_mountains_naggarond;
-                            region wh2_main_eataine_lothern;
-                            region wh2_main_avelorn_gaean_vale;
-                            region wh2_main_vampire_coast_the_awakening;
-                            region wh2_main_the_galleons_graveyard;
-                            region wh2_main_sartosa_sartosa;
-                        }
+					local mission = [[
+								mission
+								{
+										victory_type ovn_victory_type_long;
+										key wh_main_long_victory;
+										issuer CLAN_ELDERS;
+										primary_objectives_and_payload
+										{
+												objective
+												{
+												type SCRIPTED;
+												script_key ovn_hlf_collect_all_ingredients;
+												override_text mission_text_text_hlf_collect_all_ingredients_description;
+										}
 
-                            payload
-                            {
-                                game_victory;
-                            }
-                        }
-                    }
-                ]]
+												payload
+												{
+														game_victory;
+												}
+										}
+								}
+							]]
 
-                cm:trigger_custom_mission_from_string("wh2_main_emp_the_moot", mission);
+						cm:trigger_custom_mission_from_string("wh2_main_emp_the_moot", mission);
 
                    --aswad_scythans
         elseif cm:get_faction("wh2_main_nor_albion"):is_human() then
