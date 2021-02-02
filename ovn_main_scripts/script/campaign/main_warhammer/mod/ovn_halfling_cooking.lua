aeff08a75eba909022ed6795db6e2058d20bfa05
Tibor Ilić <tilic5@gmail.com>
2021-02-02 15:44:22 +0100

    fixes for the halfling cooking faction button

--- ovn_main_scripts/script/campaign/main_warhammer/mod/ovn_halfling_cooking.lua
+++ ovn_main_scripts/script/campaign/main_warhammer/mod/ovn_halfling_cooking.lua
@@ -46,6 +46,51 @@ local function remove_component(uic_obj)
     killer:DestroyChildren()
 end
 
+local function update_main_hlf_cooking_button()
+	local ui_root = core:get_ui_root()
+
+	local faction = cm:get_faction(cm:get_local_faction_name(true))
+
+	local button_group_management = find_uicomponent(core:get_ui_root(), "layout", "faction_buttons_docker", "button_group_management")
+	if not button_group_management then return end
+
+	local main_hlf_cooking_button = find_uicomponent(button_group_management, "ovn_hlf_cooking_button")
+	if not main_hlf_cooking_button then
+		main_hlf_cooking_button = UIComponent(button_group_management:CreateComponent("ovn_hlf_cooking_button", "ui/templates/round_large_button"))
+		button_group_management:Layout()
+	end
+	if not main_hlf_cooking_button then return end
+	main_hlf_cooking_button:SetVisible(true)
+
+	local cooking_interface = cm:model():world():cooking_system():faction_cooking_info(faction)
+	if not cooking_interface.active_dish then return end
+	local active_dish = cooking_interface:active_dish()
+
+	local mcc = find_ui_component_str("root > layout > faction_buttons_docker > button_group_management > button_mortuary_cult > label_mortuary_cult_count")
+	local counter = find_ui_component_str(main_hlf_cooking_button, "ovn_hlf_cooking_counter")
+	if not counter then
+		local localized_main_tooltip = effect.get_localised_string("ovn_hlf_cooking_script_hlf_cooking_main_tooltip")
+		local localized_num_turns = effect.get_localised_string("ovn_hlf_cooking_script_hlf_cooking_num_turns")
+
+		main_hlf_cooking_button:SetTooltipText(localized_main_tooltip, true)
+
+		counter = UIComponent(mcc:CopyComponent("ovn_hlf_cooking_counter"))
+		counter:SetTooltipText(localized_num_turns, true)
+		main_hlf_cooking_button:Adopt(counter:Address())
+	end
+	main_hlf_cooking_button:SetImagePath("ui/hlf_cooking4.png", 0)
+
+	if active_dish.is_null_interface then
+		main_hlf_cooking_button:StartPulseHighlight()
+		counter:SetStateText(tostring(0))
+		counter:SetVisible(true)
+	else
+		main_hlf_cooking_button:StopPulseHighlight()
+		counter:SetStateText(tostring(active_dish:remaining_duration()))
+		counter:SetVisible(true)
+	end
+end
+
 local function ui_init()
     local topbar = find_uicomponent("layout", "resources_bar", "topbar_list_parent")
     if is_uicomponent(topbar) then
@@ -76,6 +121,8 @@ local function ui_init()
                 cm:steal_escape_key(false)
 
                 core:remove_listener("halfling_close_panel")
+
+								update_main_hlf_cooking_button()
             end,
             false
         )
@@ -449,51 +496,6 @@ local function init()
     );
 end
 
-local function update_main_zoo_button()
-	local ui_root = core:get_ui_root()
-
-	local faction = cm:get_faction(cm:get_local_faction_name(true))
-
-	local button_group_management = find_uicomponent(core:get_ui_root(), "layout", "faction_buttons_docker", "button_group_management")
-	if not button_group_management then return end
-
-	local main_zoo_button = find_uicomponent(button_group_management, "ovn_hlf_cooking_button")
-	if not main_zoo_button then
-		main_zoo_button = UIComponent(button_group_management:CreateComponent("ovn_hlf_cooking_button", "ui/templates/round_large_button"))
-		button_group_management:Layout()
-	end
-	if not main_zoo_button then return end
-	main_zoo_button:SetVisible(true)
-
-	local cooking_interface = cm:model():world():cooking_system():faction_cooking_info(faction)
-	if not cooking_interface.active_dish then return end
-	local active_dish = cooking_interface:active_dish()
-
-	local mcc = find_ui_component_str("root > layout > faction_buttons_docker > button_group_management > button_mortuary_cult > label_mortuary_cult_count")
-	local counter = find_ui_component_str(main_zoo_button, "pj_zoo_counter")
-	if not counter then
-		local localized_main_tooltip = effect.get_localised_string("pj_zoo_script_zoo_main_tooltip")
-		local localized_num_turns = effect.get_localised_string("pj_zoo_script_zoo_num_turns")
-
-		main_zoo_button:SetTooltipText(localized_main_tooltip, true)
-
-		counter = UIComponent(mcc:CopyComponent("pj_zoo_counter"))
-		counter:SetTooltipText(localized_num_turns, true)
-		main_zoo_button:Adopt(counter:Address())
-	end
-	main_zoo_button:SetImagePath("ui/hlf_cooking4.png", 0)
-
-	if active_dish.is_null_interface then
-		main_zoo_button:StartPulseHighlight()
-		counter:SetStateText(tostring(0))
-		counter:SetVisible(true)
-	else
-		main_zoo_button:StopPulseHighlight()
-		counter:SetStateText(tostring(active_dish:remaining_duration()))
-		counter:SetVisible(true)
-	end
-end
-
 core:remove_listener('ovn_hlf_on_faction_turn_start_cooking')
 core:add_listener(
 	'ovn_hlf_on_faction_turn_start_cooking',
@@ -507,23 +509,7 @@ core:add_listener(
 		local local_faction_name = cm:get_local_faction_name(true)
 		if local_faction_name ~= faction_key then return end
 
-		update_main_zoo_button()
-	end,
-	true
-)
-
-core:remove_listener("ovn_hlf_on_groms_cauldron_panel_closed")
-core:add_listener(
-	"ovn_hlf_on_groms_cauldron_panel_closed",
-	"PanelClosedCampaign",
-	function(context)
-		return context.string == "groms_cauldron"
-	end,
-	function()
-		local local_faction_name = cm:get_local_faction_name(true)
-		if local_faction_name ~= faction_key then return end
-
-		update_main_zoo_button()
+		update_main_hlf_cooking_button()
 	end,
 	true
 )
@@ -531,7 +517,7 @@ core:add_listener(
 cm:add_first_tick_callback(function()
     if cm:get_local_faction_name(true) == faction_key then
         ui_init()
-				update_main_zoo_button()
+				update_main_hlf_cooking_button()
     end
 
     init()
@@ -541,5 +527,5 @@ end)
 --- We're checking for presence of execute external lua file in the traceback.
 if debug.traceback():find('pj_loadfile') then
 	ui_init()
-	update_main_zoo_button()
+	update_main_hlf_cooking_button()
 end
