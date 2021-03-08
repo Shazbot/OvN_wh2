6e4149269a56e21834ea250bf06b131490fc4f8f
Tibor Ilić <tilic5@gmail.com>
2021-03-09 00:36:51 +0100

    added an extra marauder unit to ribspreader's starting army

--- ovn_main_scripts/script/campaign/mod/sr_chaos.lua
+++ ovn_main_scripts/script/campaign/mod/sr_chaos.lua
@@ -36,7 +36,7 @@ local function sr_chaos_new_game_setup(rotblood_tribe)
 	if rotblood_tribe:is_human() then
 		cm:create_force_with_general(
 			"wh2_main_nor_rotbloods",
-			"wh_main_chs_inf_chaos_warriors_0,wh_main_chs_mon_chaos_spawn,wh_main_chs_cav_chaos_knights_0,wh_dlc08_nor_inf_marauder_berserkers_0",
+			"wh_main_chs_inf_chaos_marauders_0,wh_main_chs_inf_chaos_warriors_0,wh_main_chs_mon_chaos_spawn,wh_main_chs_cav_chaos_knights_0,wh_dlc08_nor_inf_marauder_berserkers_0",
 			"wh2_main_kingdom_of_beasts_serpent_coast",
 			583,
 			700,
@@ -128,7 +128,7 @@ local function sr_chaos_new_game_setup(rotblood_tribe)
 			)
 			cm:create_force_with_general(
 				"wh2_main_nor_rotbloods",
-				"wh_main_chs_inf_chaos_warriors_0,wh_main_chs_mon_chaos_spawn,wh_main_chs_cav_chaos_knights_0,wh_dlc08_nor_inf_marauder_berserkers_0",
+				"wh_main_chs_inf_chaos_marauders_0,wh_main_chs_inf_chaos_warriors_0,wh_main_chs_mon_chaos_spawn,wh_main_chs_cav_chaos_knights_0,wh_dlc08_nor_inf_marauder_berserkers_0",
 				"wh2_main_kingdom_of_beasts_serpent_coast",
 				563,
 				537,
