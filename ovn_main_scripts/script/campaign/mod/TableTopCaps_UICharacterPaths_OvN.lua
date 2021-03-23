a260fef353caab8d80b4bc27f01c25c2e3e4c325
Tibor Ilić <tilic5@gmail.com>
2021-03-23 10:55:27 +0100

    updated the TTC script with unique land unit keys

--- ovn_main_scripts/script/campaign/mod/TableTopCaps_UICharacterPaths_OvN.lua
+++ ovn_main_scripts/script/campaign/mod/TableTopCaps_UICharacterPaths_OvN.lua
@@ -34,3 +34,23 @@ local prefix_to_subculture = {
 for prefix, subculture in pairs(prefix_to_subculture) do
     rm:set_group_prefix_for_subculture(subculture, prefix)
 end
+
+local main_unit_to_land_units = {
+    ["ovn_boglar_no_garrison"] = "ovn_boglar_no_garrison",
+    ["ovn_shearl_no_garrison"] = "ovn_shearl_no_garrison",
+    ["wh_dlc01_chs_inf_chaos_warriors_2_no_garrison"] = "wh_dlc01_chs_inf_chaos_warriors_2",
+    ["wh_main_chs_inf_chaos_warriors_1_no_garrison"] = "wh_main_chs_inf_chaos_warriors_1",
+    ["wh_main_nor_cav_marauder_horsemen_0_no_garrison"] = "wh_main_nor_cav_marauder_horsemen_0",
+    ["wh_dlc01_chs_inf_forsaken_0_no_garrison"] = "wh_dlc01_chs_inf_forsaken_0",
+    ["elo_albion_warriors_no_garrison"] = "elo_albion_warriors",
+    ["elo_albion_warriors_spears_no_garrison"] = "elo_albion_warriors_spears",
+    ["wh2_dlc15_grn_mon_river_trolls_0_no_scrap"] = "wh2_dlc15_grn_mon_river_trolls_0",
+    ["wh2_dlc15_grn_mon_stone_trolls_0_no_scrap"] = "wh2_dlc15_grn_mon_stone_trolls_0",
+    ["ovn_troll_mon_wyvern"] = "wh2_dlc15_grn_mon_wyvern_waaagh_0"
+}--:map<string, string>
+
+rm:add_post_setup_callback(function()
+    for main_unit_key, land_unit_key in pairs(main_unit_to_land_units) do
+        rm:get_unit(main_unit_key):set_land_unit(land_unit_key)
+    end
+end)
