61276e6557b485a29bbb3595d9803d1ed6cee98f
Tibor Ilić <tilic5@gmail.com>
2021-06-10 01:16:30 +0200

    change albion storm script to not run for every player, only once on new turn

--- ovn_main_scripts/script/campaign/mod/ovn_weather.lua
+++ ovn_main_scripts/script/campaign/mod/ovn_weather.lua
@@ -1,10 +1,10 @@
-local function ovn_weather()	
+local function ovn_weather()
     if cm:model():campaign_name("main_warhammer") then
         core:add_listener(
-            "albion_weather", 
-            "FactionTurnStart", 
-            function(context) 
-                return context:faction():is_human()
+            "albion_weather",
+            "RegionTurnStart",
+            function(context)
+							return context:region():name() == "wh2_main_albion_albion"
             end,
             function(context)
                 local random_number = cm:random_number(13,1)
@@ -17,24 +17,23 @@ local function ovn_weather()
                     cm:create_storm_for_region("wh2_main_albion_isle_of_wights", 1, 1, "ovn_albion_storm")
                 end
             end,
-            true 
+            true
         )
     else
         core:add_listener(
-            "albion_weather", 
-            "FactionTurnStart", 
-            function(context) 
-                return context:faction():is_human()
+            "albion_weather",
+            "RegionTurnStart",
+            function(context)
+							return context:region():name() == "wh2_main_vor_albion_albion"
             end,
             function(context)
-
                 local random_number = cm:random_number(8,1)
 
                 if random_number == 1 then
                     cm:create_storm_for_region("wh2_main_vor_albion_albion", 1, 1, "ovn_albion_storm")
                 end
             end,
-            true 
+            true
         )
     end
 end
