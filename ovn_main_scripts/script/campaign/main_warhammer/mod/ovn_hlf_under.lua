2921effc79b799a3de6fed9d88355111f60d9691
Tibor Ilić <tilic5@gmail.com>
2021-08-24 13:05:43 +0200

    hlf lord recruitment from restaurants

--- ovn_main_scripts/script/campaign/main_warhammer/mod/ovn_hlf_under.lua
+++ ovn_main_scripts/script/campaign/main_warhammer/mod/ovn_hlf_under.lua
@@ -27,6 +27,7 @@ local function binding_iter(binding)
 end
 
 local region_labels_with_foreign_slots = {}
+local regions_with_restaurant_recruit_buttons = {}
 
 local function refresh_foreign_slots()
 	region_labels_with_foreign_slots = {}
@@ -34,6 +35,12 @@ local function refresh_foreign_slots()
 	local lf = cm:get_local_faction(true)
 	for fsm in binding_iter(lf:foreign_slot_managers()) do
 		region_labels_with_foreign_slots["label_settlement:"..fsm:region():name()] = true
+		regions_with_restaurant_recruit_buttons[fsm:region():name()] = true
+	end
+
+	for region_key, _ in pairs(regions_with_restaurant_recruit_buttons) do
+		cm:apply_effect_bundle_to_region("ovn_hlf_recruit_general", region_key, -1)
+		cm:apply_effect_bundle_to_region("ovn_hlf_recruit_wizard", region_key, -1)
 	end
 end
 
@@ -227,6 +234,187 @@ core:add_listener(
 	end,
 	function()
 		apply_campaign_ui_changes()
+	end,
+	true
+)
+
+local hlf_forenames = {
+	"858211990",
+	"858211991",
+	"858211992",
+	"858211993",
+	"858211994",
+	"858211995",
+	"858211996",
+	"858211997",
+	"858211998",
+	"858211999",
+	"858212000",
+	"858212001",
+	"858212002",
+	"858212003",
+	"858212004",
+	"858212005",
+	"858212006",
+	"858212007",
+	"858212008",
+	"858212009",
+	"858212010",
+	"858212011",
+	"858212012",
+	"858212013",
+	"858212014",
+	"858212015",
+	"858212016",
+	"858212017",
+	"858212018",
+	"858212019",
+	"858212020",
+	"858212021",
+	"858212022",
+	"858212023",
+	"858212024",
+	"858212025",
+	"858212026",
+	"858212027",
+	"858212028",
+	"858212029",
+	"858212030",
+	"858212031",
+	"858212032",
+	"858212033",
+	"858212034",
+	"858212035",
+	"858212036",
+	"858212037",
+	"858212038",
+	"858212039",
+	"858212040",
+	"858212041",
+	"858212042",
+	"858212043",
+}
+
+local hlf_surnames = {
+	"858212044",
+	"858212045",
+	"858212046",
+	"858212047",
+	"858212048",
+	"858212049",
+	"858212050",
+	"858212051",
+	"858212052",
+	"858212053",
+	"858212054",
+	"858212055",
+	"858212056",
+	"858212057",
+	"858212058",
+	"858212059",
+	"858212060",
+	"858212061",
+	"858212062",
+	"858212063",
+	"858212064",
+	"858212065",
+	"858212066",
+	"858212067",
+	"858212068",
+	"858212069",
+	"858212070",
+	"858212071",
+	"858212072",
+	"858212073",
+	"858212074",
+	"858212075",
+	"858212076",
+	"858212077",
+	"858212078",
+	"858212079",
+	"858212080",
+	"858212081",
+	"858212082",
+	"858212083",
+	"858212084",
+	"858212085",
+	"858212086",
+	"858212087",
+	"858212088",
+	"858212089",
+	"858212090",
+	"858212091",
+	"858212092",
+	"858212093",
+	"858212094",
+	"858212095",
+	"858212096",
+	"858212097",
+	"858212098",
+	"858212099",
+}
+
+core:remove_listener('ovn_hlf_recruit_general_from_restaurant')
+core:add_listener(
+	'ovn_hlf_recruit_general_from_restaurant',
+	'ComponentLClickUp',
+	function(context)
+		return context.string == "CcoEffectBundleovn_hlf_recruit_wizard_0"
+			or context.string == "CcoEffectBundleovn_hlf_recruit_general_0"
+	end,
+	function(context)
+		local subtype = "ovn_hlf_moot_general"
+		if context.string == "CcoEffectBundleovn_hlf_recruit_wizard_0" then
+			subtype = "ovn_hlf_ll"
+		end
+
+		local comp = UIComponent(context.component)
+		for _=1, 4 do
+			comp = UIComponent(comp:Parent())
+		end
+		local region_key = comp:Id():gsub("label_settlement:", "")
+
+		local x, y = cm:find_valid_spawn_location_for_character_from_settlement("wh2_main_emp_the_moot", region_key, false, false, 0)
+		if x == -1 then return end
+
+		cm:create_force_with_general(
+			"wh2_main_emp_the_moot",
+			"halfling_militia,halfling_militia,halfling_milittia_arch",
+			region_key,
+			x, y,
+			"general", subtype,
+			"names_name_"..tostring(hlf_forenames[cm:random_number(#hlf_forenames)]), "",
+			"names_name_"..tostring(hlf_surnames[cm:random_number(#hlf_surnames)]), "",
+			false,
+			function(cqi)
+				local char = cm:get_character_by_cqi(cqi)
+				cm:replenish_action_points(cm:char_lookup_str(char))
+			end
+		)
+
+		cm:remove_effect_bundle_from_region("ovn_hlf_recruit_general", region_key)
+		cm:remove_effect_bundle_from_region("ovn_hlf_recruit_wizard", region_key)
+	end,
+	true
+)
+
+core:remove_listener('ovn_hlf_restaurant_destroyed')
+core:add_listener(
+	"ovn_hlf_restaurant_destroyed",
+	"ForeignSlotManagerRemovedEvent",
+	true,
+	function(context)
+		local owner = context:owner()
+		local owner_key = owner:name()
+
+		if owner_key == "wh2_main_emp_the_moot" then
+			local region = context:region()
+			local region_name = region:name()
+			regions_with_restaurant_recruit_buttons[region_name] = nil
+			cm:remove_effect_bundle_from_region("ovn_hlf_recruit_general", region_name)
+			cm:remove_effect_bundle_from_region("ovn_hlf_recruit_wizard", region_name)
+			refresh_foreign_slots()
+		end
 	end,
 	true
 )
